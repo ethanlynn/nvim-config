b06f940204dfb9d182cc73a8572e82a6c5c84be0
Ethan Lynn <ethanlynn0709@gmail.com>
2023-08-30 10:30:04 -0400

    feat: add better C support and disable autocomplete accept on <CR>

--- lua/ethanlynn/lsp.lua
+++ lua/ethanlynn/lsp.lua
@@ -14,6 +14,7 @@ require("mason-lspconfig").setup({
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
+lspconfig.clangd.setup({ capabilitites = capabilities })
 lspconfig.lua_ls.setup({ capabilities = capabilities })
 lspconfig.pyright.setup({ capabilities = capabilities })
 lspconfig.rust_analyzer.setup({ capabilities = capabilities })
--- lua/ethanlynn/plugins/nvim-cmp.lua
+++ lua/ethanlynn/plugins/nvim-cmp.lua
@@ -20,11 +20,13 @@ return {
         },
         window = {},
         mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
